1ccd6958b6f770f0f247986daf475e3496dbb1d4
Max Shaptala <maxshaptala@gmail.com>
2020-10-04 13:38:51 +0300

    Added routing. Changed style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, createRef} from 'react';
+import React, { useState, createRef } from 'react';
+import { BrowserRouter as Router, Link, NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function Header(props) {
@@ -53,7 +54,7 @@ function StudentList(props) {
         </tr>
       </thead>
       <tbody>
-        {students.map((student, index) => <Student key={index+1} {...student} />)}
+        {students.map((student) => <Student key={student.id} {...student} />)}
       </tbody>
     </table>
   );
@@ -82,45 +83,67 @@ function StudentForm(props) {
         First Name
         <input required value={firstName} onChange={e => setFirstName(e.target.value)}/>
       </label>
-      <br/>
       <label>
         Last Name
         <input required value={lastName} onChange={e => setLastName(e.target.value)}/>
       </label>
-      <br/>
       <label>
         Age
         <input required value={age} type="number" onChange={e => setAge(e.target.value)} />
       </label>
-      <br/>
       <input type="submit"/>
     </form>
   );
 }
 
-
+function Navigation(props) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>          
+          <li><Link to="/list">List</Link></li>          
+          <li><Link to="/add">Add</Link></li>
+        </ul>
+      </nav>
+      <hr/>
+    </div>
+  );
+}
 
 function App() {
   
   const students = [
-   { firstName: "Вася", lastName: "Пупкин", age: 21 },
-   { firstName: "Маша", lastName: "Ефросинина", age: 41 },
-   { firstName: "Michael", lastName: "Jackson", age: 51 },
+   { id: 1, firstName: "Вася", lastName: "Пупкин", age: 21 },
+   { id: 2, firstName: "Маша", lastName: "Ефросинина", age: 41 },
+   { id: 3, firstName: "Michael", lastName: "Jackson", age: 51 },
   ];
+
   let [studentsState, setStudent] = useState(students);
 
   const addStudent = (student) => {
     const newList = new Array(...studentsState);
+    student.id = newList[newList.length - 1].id + 1;
     newList.push(student);
     setStudent(newList);
   }
-  
+
   return (
     <div className="App">
-      <Header>Hello, World!</Header>
-      <Button>{0}</Button>
-      <StudentForm addStudent={addStudent}/>
-      <StudentList students={studentsState}/>
+      <Router>
+        <Navigation />
+        <Header>Students</Header>   
+        <Switch>
+            <Route exact path="/" children={() => <div>Welcome to site</div>} />          
+            {/* <Button>{0}</Button> */}
+            <Route path="/add">
+              <StudentForm addStudent={addStudent} />
+            </Route>
+            <Route path="/list">
+              <StudentList students={studentsState} />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
