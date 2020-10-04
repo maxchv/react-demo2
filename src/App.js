1f616d318602459fab73b85ad516d26dd23d6b63
Max Shaptala <maxshaptala@gmail.com>
2020-10-04 10:21:16 +0300

    Showed how to use arrays

--- src/App.js
+++ src/App.js
@@ -31,11 +31,46 @@ function Button(props) {
   );
 }
 
+function Student(props) {
+  return (
+    <tr>
+      <td>{props.firstName}</td>
+      <td>{props.lastName}</td>
+      <td>{props.age}</td>
+    </tr>
+  );
+}
+
+function StudentList(props) {
+  const students = props.students;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student, index) => <Student key={index+1} {...student} />)}
+      </tbody>
+    </table>
+  );
+}
+
 function App() {
+  const students = [
+    { firstName: "Вася", lastName: "Пупкин", age: 21 },
+    { firstName: "Маша", lastName: "Ефросинина", age: 41 },
+    {firstName: "Michael", lastName: "Jackson", age: 51},
+  ];
+
   return (
     <div className="App">
       <Header>Hello, World!</Header>
       <Button>{0}</Button>
+      <StudentList students={students}/>
     </div>
   );
 }
