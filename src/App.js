607d51abeb32acb66407a34baeaba9a578f4a27b
Max Shaptala <maxshaptala@gmail.com>
2020-10-04 09:57:01 +0300

    Showed huck demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.children}</h1>
+    </header>
+  );
+}
+
+function Counter(init) {
+  let counter = init;
+  const c = function () {
+    return counter++;
+  };
+  return c;
+}
+
+function Button(props) {
+  // Хук
+  let [counter, setCounter] = useState(props.children);
+  // const c = Counter(0);
+  // let counter = props.children;
+  // const click = () => counter = c();
+  return (
+    <div>
+      <button style={{fontSize: "30px"}}
+        onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>Hello, World!</Header>
+      <Button>{0}</Button>
     </div>
   );
 }
